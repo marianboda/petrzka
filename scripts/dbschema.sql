9d335d2b39461196332555627ff20c84a5bcf821
Marian Boda <marianboda@gmail.com>
2016-06-20 07:25:28 +0200

    Merged changes from bernolakovo branch

--- scripts/dbschema.sql
+++ scripts/dbschema.sql
@@ -14,5 +14,25 @@ CREATE TABLE "ad"(
   "price_info" Text,
   "agency" Text,
   "agent" Text,
+  "images" Text,
+  "rating" Integer DEFAULT 0
+);
+
+CREATE TABLE "house"(
+  "id" Text UNIQUE,
+  "title" Text,
+  "link" Text,
+  "description" Text,
+  "area" Real,
+  "property_area" Real,
+  "type" Text,
+  "condition" Text,
+  "time_added" Text,
+  "location" Text,
+  "price" Real,
+  "price_info" Text,
+  "agency" Text,
+  "agent" Text,
+  "images" Text,
   "rating" Integer DEFAULT 0
 );
