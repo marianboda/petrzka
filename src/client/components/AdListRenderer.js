6b3131995503b8686058d0d0f266ef8d0dee148b
Marian Boda <marianboda@gmail.com>
2016-06-28 23:25:46 +0200

    Added age of ad to the list

--- src/client/components/AdListRenderer.js
+++ src/client/components/AdListRenderer.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import DOM from 'react-dom'
+import moment from 'moment'
 
-const defStyle = { cursor: 'pointer', padding: '5', position: 'relative', borderBottom: '1px solid #8AC' }
+const defStyle = { cursor: 'pointer', padding: '5', position: 'relative', borderBottom: '1px solid #8AC',
+  minHeight: 50}
 const locationStyle = {position: 'absolute', right: 10, top: 10, fontSize: 15}
+const daysOldStyle = {position: 'absolute', right: 10, bottom: 10, color: '#369', paddingLeft: 20}
 
 export default React.createClass({
   render() {
     const { data, selected, onClick } = this.props
     let style = (selected) ? {...defStyle, backgroundColor: '#AACCFF'} : defStyle
+    let days = moment().diff(moment(data.time_added, 'YYYY-MM-DD HH:mm:ss'), 'days')
+    if (days == 0) {
+      days = 'today'
+    } else if (days == 1) {
+      days = '1 day ago'
+    } else {
+      days = days + ' days ago'
+    }
     return (<div style={style} key={data.id} onClick={((id) => () => onClick(id))(data.id)}>
       <div style={{marginRight: 60}}>{data.title}</div>
       <span>{data.location}</span>
+      <span style={daysOldStyle}>{days}</span>
       <span style={locationStyle}>{data.price + data.price_energy}</span>
     </div>)
   }
